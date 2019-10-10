9631a9518c6fc95598036d8d4103c9e58fe7d6a5
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-10-10 16:41:34 +0200

    * Add graddle dependencies * Add user permission in manifest * Add compiling code

--- app/src/main/java/ch/hearc/museodio/MainActivity.kt
+++ app/src/main/java/ch/hearc/museodio/MainActivity.kt
@@ -1,11 +1,21 @@
 package ch.hearc.museodio
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.preference.PreferenceManager
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import org.osmdroid.config.Configuration
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.util.Log.d
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
@@ -13,30 +23,104 @@ import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    }
     internal var map: MapView? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         val ctx = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
-
 
         setContentView(R.layout.activity_main)
         map = findViewById<MapView>(R.id.map) as MapView
         map!!.setTileSource(TileSourceFactory.MAPNIK)
+
+        val permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (permissionState != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+        else {
+            if(isLocationEnabled()){
+                getLocationUpdates()
+            }
+            else {
+                Log.d("Localisation", "localisation is not enabled")
+            }
+        }
     }
 
     public override fun onResume() {
         super.onResume()
 
         map!!.onResume()
+
+        startLocationUpdates()
     }
 
     public override fun onPause() {
         super.onPause()
 
         map!!.onPause()
+
+        stopLocationUpdates()
     }
+
+    @SuppressLint("RestrictedApi")
+    private fun getLocationUpdates()
+    {
+        locationRequest = LocationRequest()
+            .setInterval(1000)
+            .setFastestInterval(1000)
+            .setSmallestDisplacement(100f)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                Log.d("Location", "in here")
+                locationResult ?: return
+                if (locationResult.locations.isNotEmpty()) {
+                    val location = locationResult.lastLocation
+                    Log.d("Location", location.toString())
+                }
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startLocationUpdates()
+            }
+        }
+    }
+
 }
 
