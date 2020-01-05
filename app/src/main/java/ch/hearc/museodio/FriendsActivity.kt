dbe64ad63c007b83f24b498aaffda7e9fa8ed9e3
Carraux Roxane <roxane.carraux@he-arc.ch>
2020-01-05 15:56:32 +0100

    begin friends

--- app/src/main/java/ch/hearc/museodio/FriendsActivity.kt
+++ app/src/main/java/ch/hearc/museodio/FriendsActivity.kt
@@ -11,20 +11,22 @@ import android.view.View
 import ch.hearc.museodio.api.ServiceAPI
 import ch.hearc.museodio.api.model.Users
 import kotlinx.android.synthetic.main.activity_friends.*
+import org.osmdroid.config.Configuration
 
 
-/**
- * LoginActivity : Application's entry point
- */
 class FriendsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends)
 
+
         btnTest.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View?) {
-                ServiceAPI.fetchSearchUsers(::printUsers)
+                /* ICI TODO
+
+                val bearerToken = ServiceAPI.loadApiKey()
+                ServiceAPI.fetchSearchUsers(bearerToken,::printUsers)*/
             }
         })
     }
